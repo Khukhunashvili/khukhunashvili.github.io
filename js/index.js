0c62781886c75e9fd043325aaf5dd78baa64e20c
FiNkacho <sabaxuxunashvili@gmail.com>
2019-03-23 22:34:26 +0400

    landing page

--- js/index.js
+++ js/index.js
@@ -5,3 +5,15 @@ new fullpage('#fullpage', {
   navigation: true,
   navigationPosition: 'right'
 });
+
+
+var roundLogEl = document.querySelector('.age');
+
+anime({
+  targets: roundLogEl,
+  innerHTML: [0, 18],
+  easing: 'linear',
+  round: 1 // Will round the animated value to 1 decimal
+});
+
+$(".section *").hide().slideDown(500);
