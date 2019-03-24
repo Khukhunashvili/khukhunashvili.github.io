6fcf0ba69af126d09bd99133408f22d805998e6d
FiNkacho <sabaxuxunashvili@gmail.com>
2019-03-25 01:51:18 +0400

    About me page

--- js/index.js
+++ js/index.js
@@ -9,11 +9,4 @@ new fullpage('#fullpage', {
 
 var roundLogEl = document.querySelector('.age');
 
-anime({
-  targets: roundLogEl,
-  innerHTML: [0, 18],
-  easing: 'linear',
-  round: 1 // Will round the animated value to 1 decimal
-});
-
 $(".section *").hide().slideDown(500);
